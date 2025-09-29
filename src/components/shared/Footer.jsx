ef8dea19df94b12aad0e03c9579cd63de7357ec8
Alejo3D <alejandro_insinga@hotmail.com>
2025-09-29 17:47:44 -0300

    se modifico el estilo y agregue una columna mas de prueba

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -6,38 +6,58 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container-fluid footer-container">
+      <div className="footer-container container-fluid">
         {/* Columna izquierda */}
         <div className="footer-left">
           <img
-            src="logo_fullCancha_png.png"   // <-- tu logo aquí
+            src="logo_fullCancha_png.png"
             alt="FullCancha Logo"
             className="footer-logo"
           />
           <p className="footer-copy">
-            © FullCancha 2025 - Designed & Developed by Alejo3D
+            © FullCancha 2025 - Todos los derechos reservados.
           </p>
         </div>
 
-        {/* Columna central */}
+        {/* Columna central (Enlaces) */}
         <div className="footer-center">
-          <h4 className="footer-title">Secciones</h4>
+          <h4 className="footer-title">Enlaces</h4>
           <a href="/quienesSomos">Quiénes Somos</a>
           <a href="/catalogo">Catálogo</a>
           <a href="/reservas">Reservas</a>
         </div>
 
-        {/* Columna derecha */}
+        {/* Nueva columna con enlaces de prueba */}
+        <div className="footer-extra">
+          <h4 className="footer-title">Otros</h4>
+          <a href="#">Enlace 1</a>
+          <a href="#">Enlace 2</a>
+          <a href="#">Enlace 3</a>
+        </div>
+
+        {/* Columna derecha (Redes sociales) */}
         <div className="footer-right">
-          <p>Seguinos en nuestras redes sociales</p>
+          <p>Nuestras redes sociales:</p>
           <div className="social-icons">
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="bi bi-twitter"></i>
             </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="bi bi-instagram"></i>
             </a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="bi bi-facebook"></i>
             </a>
           </div>
