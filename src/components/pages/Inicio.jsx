a041a43c15230052d3ac247f0a8a44ce35effb80
Omar Mattos <omarmatt84@gmail.com>
2025-09-28 21:12:11 -0300

    agregué las secciones nuestras canchas, productos y galería de fotos

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -11,17 +11,73 @@ const Inicio = () => {
                         src="https://images.pexels.com/photos/274506/pexels-photo-274506.jpeg"
                         alt="fondo cancha"
                     />
-                    <div
-                    className="carousel-caption position-absolute end-50 bottom-0 text-start w-50 fadeInTexto"
-                    >
-                    <h3 className="titulo fs-1 mb-0">Viví el futbol</h3>
-                    <h3 className="titulo shadow-sm">FULLCANCHAS</h3>
-                    <p className="fs-5">
-                        Aquí, la pasión por la pelota se enciende en cada partido. Tenemos el espacio perfecto para vos. Conéctate, elige tu horario y preparate para jugar.
-                    </p>
+                    <div className="carousel-caption position-absolute end-50 bottom-0 text-start w-50 fadeInTexto">
+                        <h3 className="titulo fs-1 mb-0">Viví el futbol</h3>
+                        <h3 className="titulo shadow-sm">FULLCANCHAS</h3>
+                        <p className="fs-5">
+                            Aquí, la pasión por la pelota se enciende en cada partido. Tenemos el espacio perfecto para vos. Conéctate, elige tu horario y preparate para jugar.
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <Container className="mt-5">
+                <h1>Nuestras Canchas</h1>
+                <Row>
+                    {
+
+                    }
+                </Row>
+                <h2>Shopp</h2>
+                <Row>
+                    {
+
+                    }
+                </Row>
+            </Container>
+            <Row className="mt-4">
+                <Col xs={12} md={4} lg={2} className="p-0">
+                    <img
+                        className="imagenGaleria w-100"
+                        src="https://images.pexels.com/photos/274422/pexels-photo-274422.jpeg"
+                        alt="imagen jugadores"
+                    />
+                </Col>
+                <Col xs={12} md={4} lg={2} className="p-0">
+                    <img
+                        className="imagenGaleria w-100"
+                        src="https://images.pexels.com/photos/1378425/pexels-photo-1378425.jpeg"
+                        alt="cancha 1"
+                    />
+                </Col>
+                <Col xs={12} md={4} lg={2} className="p-0">
+                    <img
+                        className="imagenGaleria w-100"
+                        src="https://images.pexels.com/photos/774321/pexels-photo-774321.jpeg"
+                        alt="botin futbol"
+                    />
+                </Col>
+                <Col xs={12} md={4} lg={2} className="p-0">
+                    <img
+                        className="imagenGaleria w-100"
+                        src="https://images.pexels.com/photos/114296/pexels-photo-114296.jpeg"
+                        alt="cancha atardecer"
+                    />
+                </Col>
+                <Col xs={12} md={4} lg={2} className="p-0">
+                    <img
+                        className="imagenGaleria w-100"
+                        src="https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg"
+                        alt="cesped futbol pelota"
+                    />
+                </Col>
+                <Col xs={12} md={4} lg={2} className="p-0">
+                    <img
+                        className="imagenGaleria w-100"
+                        src="https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg"
+                        alt="dominio pelota futbol"
+                    />
+                </Col>
+            </Row>
         </section>
     );
 };
