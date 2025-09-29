7bf678eaccae08a9d99a1c02440170f47473b91f
Omar Mattos <omarmatt84@gmail.com>
2025-09-28 21:01:41 -0300

    se agregó una imagen como banner y un eslogan con su correspondientes estilos css

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,8 +1,28 @@
+import { Container, Row, Col, Form } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
 const Inicio = () => {
     return (
-        <div>
-            <h1>Inicio</h1>
+        <section className="mainSection">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img
+                        className="banner"
+                        src="https://images.pexels.com/photos/274506/pexels-photo-274506.jpeg"
+                        alt="fondo cancha"
+                    />
+                    <div
+                    className="carousel-caption position-absolute end-50 bottom-0 text-start w-50 fadeInTexto"
+                    >
+                    <h3 className="titulo fs-1 mb-0">Viví el futbol</h3>
+                    <h3 className="titulo shadow-sm">FULLCANCHAS</h3>
+                    <p className="fs-5">
+                        Aquí, la pasión por la pelota se enciende en cada partido. Tenemos el espacio perfecto para vos. Conéctate, elige tu horario y preparate para jugar.
+                    </p>
+                </div>
+            </div>
         </div>
+        </section>
     );
 };
 
