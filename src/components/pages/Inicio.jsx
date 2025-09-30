45599661ad52179d4bf1657e024ee76b286c511f
Omar Mattos <omarmatt84@gmail.com>
2025-09-30 18:21:02 -0300

    cambié el color de fondo del main, efectos de sombras en los títulos y modifique el maquetado del instructivo para la reserva

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,9 +1,31 @@
-import { Container, Row, Col, Form } from "react-bootstrap";
+import { Container, Row, Col, Form, Modal } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import CardCancha from "./cancha/CardCancha";
 import CardProducto from "./producto/CardProducto";
 
 const Inicio = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedImage, setSelectedImage] = useState("");
+
+    const galleryImages = [
+        "https://images.pexels.com/photos/274422/pexels-photo-274422.jpeg",
+        "https://images.pexels.com/photos/1378425/pexels-photo-1378425.jpeg",
+        "https://images.pexels.com/photos/774321/pexels-photo-774321.jpeg",
+        "https://images.pexels.com/photos/114296/pexels-photo-114296.jpeg",
+        "https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg",
+        "https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg"
+    ];
+
+    const handleImageClick = (imageUrl) => {
+        setSelectedImage(imageUrl);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSelectedImage("");
+    };
+
     return (
         <section className="mainSection">
             <div className="carousel-inner">
@@ -14,8 +36,8 @@ const Inicio = () => {
                         alt="fondo cancha"
                     />
                     <div className="carousel-caption position-absolute end-50 bottom-0 text-start w-50 fadeInTexto">
-                        <h3 className="titulo fs-1 mb-0 fw-light">Viví el futbol</h3>
-                        <h3 className="titulo shadow-sm fw-bold">FULLCANCHAS</h3>
+                        <h3 className="titulo-banner fs-1 mb-0 fw-light">Viví el futbol</h3>
+                        <h3 className="titulo-banner shadow-sm fw-bold">FULLCANCHAS</h3>
                         <p className="fs-5 fw-light">
                             Aquí, la pasión por la pelota se enciende en cada partido. Tenemos el espacio perfecto para vos. Conéctate, elige tu horario y preparate para jugar.
                         </p>
@@ -23,9 +45,12 @@ const Inicio = () => {
                 </div>
             </div>
             <section>
-                <Container className="mt-2">
-                    <Row className="bg-shopp pt-3 rounded-2 mb-4">
+                <div className="bg-shopp">
+                    <h3 className="container text-white text-center fs-5 pt-3 mb-0">Reservá tu cancha en 3 simples pasos</h3>
+                <Container>
+                    <Row className=" pt-3 rounded-2 mb-4">
                         <Col className="d-flex flex-column align-items-center mx-4">
+                        <p className="fw-bold fs-5 text-white">PASO 1</p>
                             <img
                                 className="imagen-icono"
                                 src="https://res.cloudinary.com/duwi53e7z/image/upload/v1759154741/Elegir2_t2ykcl.png"
@@ -34,6 +59,7 @@ const Inicio = () => {
                             <p className="fw-bolder fs-5 text-white">Elegí tu cancha</p>
                         </Col>
                         <Col className="d-flex flex-column align-items-center mx-4">
+                            <p className="fw-bold fs-5 text-white">PASO 2</p>
                             <img
                                 className="imagen-icono"
                                 src="https://res.cloudinary.com/duwi53e7z/image/upload/v1759154741/Horario2_satb47.png"
@@ -42,6 +68,7 @@ const Inicio = () => {
                             <p className="fw-bolder fs-5 text-white">Elegí el día y la hora</p>
                         </Col>
                         <Col className="d-flex flex-column align-items-center mx-4">
+                            <p className="fw-bold fs-5 text-white">PASO 3</p>
                             <img
                                 className="imagen-icono"
                                 src="https://res.cloudinary.com/duwi53e7z/image/upload/v1759154741/Juga2_xw0qnm.png"
@@ -50,7 +77,11 @@ const Inicio = () => {
                             <p className="fw-bolder fs-5 text-white">Vení a Jugar!!!</p>
                         </Col>
                     </Row>
-                    <h1 className="text-center">Nuestras Canchas</h1>
+                </Container>
+                </div>
+                
+                <Container className="mt-2">
+                    <h1 className="titulo-seccion text-center mt-5 mb-4 text-white">Nuestras Canchas</h1>
                     <Row className="justify-content-center">
                         <CardCancha></CardCancha>
                         <CardCancha></CardCancha>
@@ -58,65 +89,56 @@ const Inicio = () => {
                         <CardCancha></CardCancha>
                         <CardCancha></CardCancha>
                     </Row>
-                    <div className="bg-shopp px-5 rounded-4 mb-1">
-                        <h2 className="text-center text-white">Productos</h2>
-                        <Row>
-                        <CardProducto></CardProducto>
-                        </Row>
-                    </div>
+
 
                 </Container>
+                    <div className="bg-shopp px-5  pt-3">
+                        <h2 className="titulo-seccion text-center mt-3 mb-4 text-white fs-1">Productos</h2>
+                        <Container>
+                        <Row>
+                            <CardProducto></CardProducto>
+                            <CardProducto></CardProducto>
+                            <CardProducto></CardProducto>
+                        </Row>
+                        </Container>
+                    </div>
             </section>
 
-            <Row className="mt-2">
-                <Col xs={12} md={4} lg={2} className="p-0">
-                    <img
-                        className="imagenGaleria w-100"
-                        src="https://images.pexels.com/photos/274422/pexels-photo-274422.jpeg"
-                        alt="imagen jugadores"
-                    />
-                </Col>
-                <Col xs={12} md={4} lg={2} className="p-0">
-                    <img
-                        className="imagenGaleria w-100"
-                        src="https://images.pexels.com/photos/1378425/pexels-photo-1378425.jpeg"
-                        alt="cancha 1"
-                    />
-                </Col>
-                <Col xs={12} md={4} lg={2} className="p-0">
-                    <img
-                        className="imagenGaleria w-100"
-                        src="https://images.pexels.com/photos/774321/pexels-photo-774321.jpeg"
-                        alt="botin futbol"
-                    />
-                </Col>
-                <Col xs={12} md={4} lg={2} className="p-0">
-                    <img
-                        className="imagenGaleria w-100"
-                        src="https://images.pexels.com/photos/114296/pexels-photo-114296.jpeg"
-                        alt="cancha atardecer"
-                    />
-                </Col>
-                <Col xs={12} md={4} lg={2} className="p-0">
-                    <img
-                        className="imagenGaleria w-100"
-                        src="https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg"
-                        alt="cesped futbol pelota"
-                    />
-                </Col>
-                <Col xs={12} md={4} lg={2} className="p-0">
-                    <img
-                        className="imagenGaleria w-100"
-                        src="https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg"
-                        alt="dominio pelota futbol"
-                    />
-                </Col>
+            <Row className="mt-0">
+                {galleryImages.map((imageUrl, index) => (
+                    <Col key={index} xs={12} md={4} lg={2} className="p-0">
+                        <img
+                            className="contenedor-imagen imagenGaleria w-100"
+                            src={imageUrl}
+                            alt={`imagen galería ${index + 1}`}
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => handleImageClick(imageUrl)}
+                        />
+                    </Col>
+                ))}
             </Row>
+
+            <Modal 
+                show={showModal} 
+                onHide={handleCloseModal} 
+                size="lg" 
+                centered
+                className="image-modal"
+            >
+                <Modal.Body className="text-center p-0">
+                    <img 
+                        src={selectedImage} 
+                        alt="Vista ampliada" 
+                        className="modal-imagen-ampliada"
+                    />
+                </Modal.Body>
+            </Modal>
+            
             <div className="container mt-5 ">
-                <h3 >Donde estamos</h3>
+                <h3 className="titulo-seccion text-white">Donde estamos</h3>
                 <div className="d-flex align-items-center">
-                    <i className="bi bi-geo-alt fs-2"></i>
-                    <p className="fs-6 mx-2 mb-0 fw-light">Gral. José María Paz 1544, San Miguel de Tucumán, Tucumán</p>
+                    <i className="bi bi-geo-alt fs-2 text-white"></i>
+                    <p className="fs-6 mx-2 mb-0 fw-light text-white">Gral. José María Paz 1544, San Miguel de Tucumán, Tucumán</p>
                 </div>
             </div>
             <div >
