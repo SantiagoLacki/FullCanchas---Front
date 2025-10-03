7de313b8143d099e4ad32fb0fd85ea3888a50aa1
Omar Mattos <omarmatt84@gmail.com>
2025-10-03 20:50:45 -0300

    agregué un dropdown para poder elegir entre las distintas secciones de la pagina

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,4 +1,4 @@
-import { Col, Form, Row, Table } from "react-bootstrap";
+import { Col, Dropdown, Form, Row, Table } from "react-bootstrap";
 import { Link } from "react-router";
 import ItemUsuario from "./usuario/ItemUsuario";
 import { useState } from "react";
@@ -6,6 +6,7 @@ import ItemCancha from "./cancha/ItemCancha";
 import ItemProducto from "./producto/ItemProducto";
 
 const Administrador = () => {
+    const [activeSection, setActiveSection] = useState("usuarios");
     const [terminoBusqueda, setTerminoBusqueda] = useState('')
 
     const handleBuscarChange=(e)=>{
@@ -14,54 +15,88 @@ const Administrador = () => {
 
     return (
         <section className="container mainSection">
+            <Dropdown className="mt-5">
+                <Dropdown.Toggle variant="gold" className="text-white btn-gold">
+                    {activeSection === 'usuarios' && 'Usuarios'}
+                    {activeSection === 'canchas' && 'Canchas'}
+                    {activeSection === 'productos' && 'Productos'}
+                </Dropdown.Toggle>
 
-            <div className="d-flex justify-content-between align-items-center mt-5 mb-3">
-                <div className="d-flex justify-content-between align-items-center">
-                    <h2 className="display-6 titulo-banner fw-bold text-white me-4">Usuarios</h2>
+                <Dropdown.Menu>
+                    <Dropdown.Item 
+                        onClick={() => setActiveSection('usuarios')}
+                        active={activeSection === 'usuarios'}
+                        className={activeSection === 'usuarios' ? 'btn-gold' : ''}
+                    >
+                        Usuarios
+                    </Dropdown.Item>
+                    <Dropdown.Item 
+                        onClick={() => setActiveSection('canchas')}
+                        active={activeSection === 'canchas'}
+                        className={activeSection === 'canchas' ? 'btn-gold' : ''}
+                    >
+                        Canchas
+                    </Dropdown.Item>
+                    <Dropdown.Item 
+                        onClick={() => setActiveSection('productos')}
+                        active={activeSection === 'productos'}
+                        className={activeSection === 'productos' ? 'btn-gold' : ''}
+                    >
+                        Productos
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+           {activeSection === 'usuarios' && ( 
+            <div>
+                <div className="d-flex justify-content-between align-items-center mt-5 mb-3">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h2 className="display-6 titulo-banner fw-bold text-white me-4">Usuarios</h2>
+                        <div>
+                        <Link className="btn btn-gold text-white" to={'/administrador/crear'} ><i class="bi bi-plus-circle"></i> Agregar     
+                        </Link>
+                        </div>
+                    </div>
                     <div>
-                    <Link className="btn btn-gold text-white" to={'/administrador/crear'} ><i class="bi bi-plus-circle"></i> Agregar     
-                    </Link>
+                        <Form>
+                            <Row className="d-flex justify-content-start">
+                                <Col xs="auto d-flex">
+                                    <i className="bi bi-search fs-3 me-2 text-secondary"></i>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Buscar"
+                                        className=" mr-sm-2"
+                                        onChange={handleBuscarChange}
+                                        value={terminoBusqueda}
+                                    />
+                                </Col>
+                            </Row>
+                        </Form>
                     </div>
                 </div>
-                <div>
-                    <Form>
-                        <Row className="d-flex justify-content-start">
-                            <Col xs="auto d-flex">
-                                <i className="bi bi-search fs-3 me-2 text-secondary"></i>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Buscar"
-                                    className=" mr-sm-2"
-                                    onChange={handleBuscarChange}
-                                    value={terminoBusqueda}
-                                />
-                            </Col>
-                        </Row>
-                    </Form>
-                </div>
+                <Table responsive striped bordered hover>
+                    <thead>
+                    <tr className="text-center">
+                        <th className="text-secondary">#</th>
+                        <th className="text-secondary">Nombre de Usuario</th>
+                        <th className="text-secondary">Email</th>
+                        <th className="text-secondary">Acciones</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        <>
+                            <ItemUsuario></ItemUsuario>
+                            <ItemUsuario></ItemUsuario>
+                            <ItemUsuario></ItemUsuario>
+                            <ItemUsuario></ItemUsuario>
+                            <ItemUsuario></ItemUsuario>
+                        </>
+                    }
+                    </tbody>
+                </Table>
             </div>
-            <Table responsive striped bordered hover>
-                <thead>
-                <tr className="text-center">
-                    <th className="text-secondary">#</th>
-                    <th className="text-secondary">Nombre de Usuario</th>
-                    <th className="text-secondary">Email</th>
-                    <th className="text-secondary">Acciones</th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    <>
-                        <ItemUsuario></ItemUsuario>
-                        <ItemUsuario></ItemUsuario>
-                        <ItemUsuario></ItemUsuario>
-                        <ItemUsuario></ItemUsuario>
-                        <ItemUsuario></ItemUsuario>
-                    </>
-                }
-                </tbody>
-            </Table>
-
+            )}
+            {activeSection === 'canchas' && ( 
             <div>
                 <div className="d-flex align-items-center mt-5 mb-3">
                     <h2 className="display-6 titulo-banner fw-bold text-white me-4">Canchas</h2>
@@ -94,14 +129,32 @@ const Administrador = () => {
                     </tbody>
                 </Table>
             </div>
-
+            )}
+            {activeSection === 'productos' && ( 
             <div>
-                <div className="d-flex align-items-center mt-5 mb-3">
-                    <h2 className="display-6 titulo-banner fw-bold text-white me-4">Productos</h2>
+                <div className="d-flex justify-content-between align-items-center mt-5 mb-3">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h2 className="display-6 titulo-banner fw-bold text-white me-4">Productos</h2>
+                        <div>
+                        <Link className="btn btn-gold text-white" to={'/administrador/crear'} ><i class="bi bi-plus-circle"></i> Agregar     
+                        </Link>
+                        </div>
+                    </div>
                     <div>
-                    <Link className="btn btn-gold text-white" to={'/administrador/crear'} >
-                        <i class="bi bi-plus-circle"></i> Agregar
-                    </Link>
+                        <Form>
+                            <Row className="d-flex justify-content-start">
+                                <Col xs="auto d-flex">
+                                    <i className="bi bi-search fs-3 me-2 text-secondary"></i>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Buscar"
+                                        className=" mr-sm-2"
+                                        onChange={handleBuscarChange}
+                                        value={terminoBusqueda}
+                                    />
+                                </Col>
+                            </Row>
+                        </Form>
                     </div>
                 </div>
                 <Table responsive striped bordered hover>
@@ -128,6 +181,7 @@ const Administrador = () => {
                     </tbody>
                 </Table>
             </div>
+            )}
         </section> 
     );
 };
