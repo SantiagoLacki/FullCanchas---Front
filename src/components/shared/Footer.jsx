74573452470b03e514195b4bebe6d0873f65aec1
Alejo3D <alejandro_insinga@hotmail.com>
2025-10-02 21:27:04 -0300

    se probo diseño nuevo de footer .2.0

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,4 +1,3 @@
-// src/components/shared/Footer.jsx
 import React from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./Footer.css";
@@ -6,60 +5,49 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-container container-fluid">
-        {/* Columna izquierda */}
-        <div className="footer-left">
+      {/* Fila con 3 columnas */}
+      <div className="footer-middle container-fluid">
+        {/* Columna izquierda: enlaces + redes */}
+        <div className="footer-col footer-left">
+          <h4 className="footer-title">Enlaces</h4>
+          <a href="/quienesSomos">Quiénes Somos</a>
+          <a href="/catalogo">Catálogo</a>
+          <a href="/reservas">Reservas</a>
+
+          <p className="footer-social-title">Nuestras redes</p>
+          <div className="social-icons">
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-twitter"></i>
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-instagram"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-facebook"></i>
+            </a>
+          </div>
+        </div>
+
+        {/* Columna central: logo + copyright */}
+        <div className="footer-col footer-center">
           <img
             src="logo_fullCancha_png.png"
             alt="FullCancha Logo"
             className="footer-logo"
           />
           <p className="footer-copy">
-            © FullCancha 2025 - Todos los derechos reservados.
+            © FullCancha 2025 - Designed & Developed by Alejo3D
           </p>
         </div>
 
-        {/* Columna central (Enlaces) */}
-        <div className="footer-center">
-          <h4 className="footer-title">Enlaces</h4>
-          <a href="/quienesSomos">Quiénes Somos</a>
-          <a href="/catalogo">Catálogo</a>
-          <a href="/reservas">Reservas</a>
-        </div>
-
-        {/* Nueva columna con enlaces de prueba */}
-        <div className="footer-extra">
-          <h4 className="footer-title">Otros</h4>
-          <a href="#">Enlace 1</a>
-          <a href="#">Enlace 2</a>
-          <a href="#">Enlace 3</a>
-        </div>
-
-        {/* Columna derecha (Redes sociales) */}
-        <div className="footer-right">
-          <p>Nuestras redes sociales:</p>
-          <div className="social-icons">
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-twitter"></i>
-            </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-instagram"></i>
-            </a>
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-facebook"></i>
-            </a>
+        {/* Columna derecha: logos de marcas */}
+        <div className="footer-col footer-right">
+          <h4 className="footer-title">Auspician</h4>
+          <div className="sponsor-logos">
+            <img src="png-adidas-logo.png" alt="Sponsor 1" className="logo-adidas" />
+            <img src="png-nike-logo.png" alt="Sponsor 2" />
+            <img src="png-puma-logo.png" alt="Sponsor 3" />
+            <img src="logo_lig_tucu.png" alt="Liga Tucumana" />
           </div>
         </div>
       </div>
