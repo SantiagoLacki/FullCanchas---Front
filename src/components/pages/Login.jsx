2024f2f338868158d270ccac3ad04742c25fca1e
Santiago Lacki <santiagolacki@gmail.com>
2025-10-03 18:05:23 -0300

    Se agrego la estructira del formulario del login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,25 @@
-const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import { Row, Col, Form, Button } from 'react-bootstrap';
+
+function Login() {
+  return (
+    <section className='container my-3'>
+      <h1>Inicio de Sesión</h1>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Correo Electronico: </Form.Label>
+              <Form.Control type="email" placeholder="nombre@dominio.com" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Contraseña: </Form.Label>
+              <Form.Control type="pasword" placeholder="Contraseña" />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </section>
+  );
+}
 
 export default Login;
