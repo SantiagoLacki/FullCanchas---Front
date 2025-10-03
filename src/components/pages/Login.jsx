527bb594ade23af582a3ec06b8a7ed2f0c226493
Santiago Lacki <santiagolacki@gmail.com>
2025-10-03 19:23:19 -0300

    Se agrego la foto del login y la distribucion de las columnas

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -3,10 +3,10 @@ import { Row, Col, Form, Button } from 'react-bootstrap';
 function Login() {
   return (
     <section className='container my-3'>
-      <h1>Inicio de Sesión</h1>
-      <Row>
+      <h1 className='text-center my-5'>Inicio de Sesión</h1>
+      <Row xs={1} md={2} className='mt-5'>
         <Col>
-          <Form>
+          <Form >
             <Form.Group className="mb-3">
               <Form.Label>Correo Electronico: </Form.Label>
               <Form.Control type="email" placeholder="nombre@dominio.com" />
@@ -16,6 +16,9 @@ function Login() {
               <Form.Control type="pasword" placeholder="Contraseña" />
             </Form.Group>
           </Form>
+        </Col>
+        <Col>
+          <img src="https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg" alt="pelota" className="w-100 rounded d-none d-sm-block"></img>
         </Col>
       </Row>
     </section>
