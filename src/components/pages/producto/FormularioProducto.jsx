6c7b0bb3ea3ebdf490e8785f20ca824fb0dcc272
Omar Mattos <omarmatt84@gmail.com>
2025-10-07 20:55:12 -0300

    agregé una imagen y la sección donde irá el formulario con el título

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { Form, Button} from "react-bootstrap";
+import Swal from 'sweetalert2'
+import { Link, useNavigate, useParams } from "react-router";
+import { useEffect } from "react";
+import { useForm} from "react-hook-form";
 
 const FormularioProducto = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="text-center mt-3">
+                <img
+                    className="imagen-icono"
+                    src="https://res.cloudinary.com/duwi53e7z/image/upload/v1759706584/producto_lnjcpd.png"
+                    alt="icono elegí"
+                />
+            </div> 
+            <section className="container mainSection border text-white rounded-2 py-1 px-4 mt-4 shadow-lg">
+                <h1 className="display-6 titulo-banner fw-bold text-center me-4 mt-2">Nuevo Producto</h1>
+
+            </section>
+        </>
     );
 };
 
