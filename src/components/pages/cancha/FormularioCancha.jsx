3b61729656b8c2c3f4579b1524d83819969e460c
Omar Mattos <omarmatt84@gmail.com>
2025-10-07 18:29:43 -0300

    agregué título, el Form, el useForm y la estructura de la función onSubmit

--- src/components/pages/cancha/FormularioCancha.jsx
+++ src/components/pages/cancha/FormularioCancha.jsx
@@ -5,6 +5,19 @@ import { useEffect } from "react";
 import { useForm} from "react-hook-form";
 
 const FormularioCancha = () => {
+    const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors },
+      setValue
+    } = useForm();
+    const navegacion = useNavigate()
+    const {id} = useParams()
+
+    const onSubmit = async (receta) =>{
+
+    }
     return (
         <>
             <div className="text-center mt-3">
@@ -14,7 +27,14 @@ const FormularioCancha = () => {
                     alt="icono cancha"
                 />
             </div>
-            
+            <section className="container mainSection border text-white rounded-2 py-1 px-4 mt-4 shadow-lg">
+                <h1 className="display-6 titulo-banner fw-bold text-center me-4 mt-2">Nueva Cancha</h1>
+                <div className="d-flex justify-content-center">
+                    <Form className="my-4 w-75" onSubmit={handleSubmit(onSubmit)}>
+
+                    </Form>
+                </div>
+            </section>
         </>
     );
 };
