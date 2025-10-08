65153682112cf22afe72c154526b1bf62afde78c
Omar Mattos <omarmatt84@gmail.com>
2025-10-07 21:37:13 -0300

    agregué botones en las celdas donde se mostrará si el turno está o no reservado

--- src/components/pages/reserva/ItemReserva.jsx
+++ src/components/pages/reserva/ItemReserva.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import { Button } from "react-bootstrap";
+import { Link } from "react-router";
 
 const ItemReserva = () => {
     return (
-        <div>
-            
-        </div>
+        <tr>
+            <td className="text-center align-middle fw-light">18:00</td>
+            <td className="align-middle fw-light text-center">
+                <Link className="btn btn-reservar border-0 text-white w-100" to={'/reserva'}>
+                    Reservar
+                </Link>
+            </td>
+            <td className="align-middle fw-light text-center">
+                <Link className="btn btn-reservar border-0 text-white w-100" to={'/reserva/'}>
+                    Reservar
+                </Link>
+            </td>
+            <td className="align-middle fw-light text-center">
+                <Link className="btn btn-reservado border-0 text-white w-100" to={'/reserva/'}>
+                    Reservado
+                </Link>
+            </td>
+            <td className="align-middle fw-light text-center">
+                <Link className="btn btn-reservar border-0 text-white w-100" to={'/reserva/'}>
+                    Reservar
+                </Link>
+            </td>
+            <td className="align-middle fw-light text-center">
+                <Link className="btn btn-reservado border-0 text-white w-100" to={'/reserva/'}>
+                    Reservado
+                </Link>
+            </td>
+            <td className="align-middle fw-light text-center">
+                <Link className="btn btn-reservar border-0 text-white w-100" to={'/reserva/'}>
+                    Reservar
+                </Link>
+            </td>
+        </tr>
     );
 };
 
