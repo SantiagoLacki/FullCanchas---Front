936e28cfedc6ef40471ae751c853e6048fb36703
Omar Mattos <omarmatt84@gmail.com>
2025-10-12 17:18:00 -0300

    podemos ver el detalle de cada producto en la grilla del administrador

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -1,25 +1,27 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router";
+import { borrarProductoPorId, leerProductos } from "../helpers/queries";
+import Swal from "sweetalert2";
 
-const ItemProducto = () => {
-    const eliminarProducto = ()=>{
+const ItemProducto = ({producto, fila, setListaProductos}) => {
+    const eliminarProducto=()=>{
 
-    }
+        }
     return (
         <tr>
-            <td className="text-center align-middle fw-light">1</td>
-            <td className="align-middle fw-light">Pelota de Futbol 5 Adidas</td>
-            <td className="text-center align-middle fw-light">$150.000</td>
-            <td className="text-center align-middle fw-light">Pelotas</td>
+            <td className="text-center align-middle fw-light">{fila}</td>
+            <td className="align-middle fw-light">{producto.nombre}</td>
+            <td className="text-center align-middle fw-light">{producto.precio}</td>
+            <td className="text-center align-middle fw-light">{producto.categoria}</td>
             <td className="text-center align-middle">
                 <img
-                    src="https://images.pexels.com/photos/32675206/pexels-photo-32675206.jpeg"
+                    src={producto.imagen}
                     className="img-thumbnail"
                     alt="cancha"
                 ></img>
             </td>
             <td className="text-center align-middle">
-                <Link className="me-lg-2 btn btn-gold text-white" to={'/usuarios/editar/'}>
+                <Link className="me-lg-2 btn btn-gold text-white" to={'/administrador/editarproducto/'+producto._id}>
                 <i className="bi bi-pencil-square"></i>
                 </Link>
                 <Button variant="danger" onClick={eliminarProducto}>
