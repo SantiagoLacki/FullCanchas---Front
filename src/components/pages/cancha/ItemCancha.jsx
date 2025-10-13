cb2f094fd9e975b02e200b72861a580c4c506499
Omar Mattos <omarmatt84@gmail.com>
2025-10-13 09:42:50 -0300

    se agregó la logica necesaria para eliminar una cancha

--- src/components/pages/cancha/ItemCancha.jsx
+++ src/components/pages/cancha/ItemCancha.jsx
@@ -5,6 +5,36 @@ import { borrarCanchaPorId, leerCanchas } from "../helpers/queries";
 
 const ItemCancha = ({cancha, fila, setListaCanchas}) => {
     const eliminarCancha=()=>{
+         Swal.fire({
+            title: "Eliminar Cancha",
+            text: "No puedes revertir este paso",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#024959",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Eliminar",
+            cancelButtonText: "Cancelar"
+            }).then(async(result) => {
+            if (result.isConfirmed) {
+                const respuesta = await borrarCanchaPorId(cancha._id)
+                if(respuesta.status === 200){
+                Swal.fire({
+                    title: "Cancha eliminada",
+                    text: `La Cancha ${cancha.nombre} fue eliminada correctamente`,
+                    icon: "success",
+                });
+                const respuestaCanchas = await leerCanchas();
+                const canchasActualizadas = await respuestaCanchas.json()
+                setListaCanchas(canchasActualizadas)
+                }else{
+                    Swal.fire({
+                    title: "Ocurrio un error",
+                    text: `La canchas ${cancha.nombre} no pudo ser eliminada.`,
+                    icon: "error",
+                });
+                }
+            }
+            });
     }
     return (
         <tr>
