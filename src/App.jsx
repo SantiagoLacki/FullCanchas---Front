0d8088c91c303e456981463440cc84e4561532e8
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-10-16 09:23:45 -0300

    agregue la navbar la pagina catalogo y a prettier para que las lineas de codigo se quebren despues de las 140 caracteres

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Menu from "./components/shared/Navbar";
 import Footer from "./components/shared/Footer";
-import Administrador from "./components/pages/Administrador"
-import Error404 from "./components/pages/Error404"
-import Inicio from "./components/pages/Inicio"
-import Login from "./components/pages/Login"
-import QuienesSomos from "./components/pages/QuienesSomos"
-import Register from "./components/pages/Register"
+import Administrador from "./components/pages/Administrador";
+import Error404 from "./components/pages/Error404";
+import Inicio from "./components/pages/Inicio";
+import Login from "./components/pages/Login";
+import QuienesSomos from "./components/pages/QuienesSomos";
+import Register from "./components/pages/Register";
 import FormularioUsuario from "./components/pages/usuario/FormularioUsuario";
 import FormularioCancha from "./components/pages/cancha/FormularioCancha";
 import ReservaCancha from "./components/pages/reserva/ReservaCancha";
@@ -14,48 +14,47 @@ import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import { useEffect, useState } from "react";
 import ProtectorAdmin from "./components/routes/ProtectorAdmin";
 import FormularioReserva from "./components/pages/reserva/FormularioReserva";
-import PopupAd from './components/shared/Publicidad/Publicidad.jsx'; // Importar el componente
+import PopupAd from "./components/shared/Publicidad/Publicidad.jsx"; // Importar el componente
 import Productos from "./components/pages/Productos.jsx";
 
 function App() {
-  const usuarioLogueado = JSON.parse(sessionStorage.getItem('userKey')) || {}
-  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado)
+  const usuarioLogueado = JSON.parse(sessionStorage.getItem("userKey")) || {};
+  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado);
 
-  useEffect(()=>{
-    sessionStorage.setItem('userKey', JSON.stringify(usuarioAdmin))
-  }, [usuarioAdmin])
+  useEffect(() => {
+    sessionStorage.setItem("userKey", JSON.stringify(usuarioAdmin));
+  }, [usuarioAdmin]);
 
   return (
     <>
-    <BrowserRouter>
-      <Menu usuarioAdmin={usuarioAdmin} setUsuarioAdmin={setUsuarioAdmin}></Menu>
-      <main>
-        <PopupAd/>
-        <Routes>
-          <Route path="/" element={<Inicio></Inicio>}></Route>
-          <Route path="/productos" element={<Productos></Productos>}></Route>
-          <Route path="/login" element={<Login setUsuarioAdmin={setUsuarioAdmin}></Login>}></Route>
-          <Route path="/quienesSomos" element={<QuienesSomos></QuienesSomos>}></Route>
-          <Route path="/registro" element={<Register></Register>}></Route>
-          <Route path="/administrador" element={<ProtectorAdmin isAdmin={usuarioAdmin}></ProtectorAdmin>}>
+      <BrowserRouter>
+        <Menu usuarioAdmin={usuarioAdmin} setUsuarioAdmin={setUsuarioAdmin}></Menu>
+        <main>
+          <PopupAd />
+          <Routes>
+            <Route path="/" element={<Inicio></Inicio>}></Route>
+            <Route path="/productos" element={<Productos></Productos>}></Route>
+            <Route path="/login" element={<Login setUsuarioAdmin={setUsuarioAdmin}></Login>}></Route>
+            <Route path="/quienesSomos" element={<QuienesSomos></QuienesSomos>}></Route>
+            <Route path="/registro" element={<Register></Register>}></Route>
+            <Route path="/administrador" element={<ProtectorAdmin isAdmin={usuarioAdmin}></ProtectorAdmin>}>
               <Route index element={<Administrador usuarioAdmin={usuarioAdmin}></Administrador>}></Route>
-              <Route path="crearusuario" element={<FormularioUsuario titulo={'Usuario Nuevo'}></FormularioUsuario>}></Route>
-              <Route path="editarusuario/:id" element={<FormularioUsuario titulo={'Modificar Usuario'}></FormularioUsuario>}></Route>
-              <Route path="crearcancha" element={<FormularioCancha titulo={'Cancha Nueva'}></FormularioCancha>}></Route>
-              <Route path="editarcancha/:id" element={<FormularioCancha titulo={'Modificar Cancha'}></FormularioCancha>}></Route>
-              <Route path="crearproducto" element={<FormularioProducto titulo={'Producto Nuevo'}></FormularioProducto>}></Route>
-              <Route path="editarproducto/:id" element={<FormularioProducto titulo={'Modificar Producto'}></FormularioProducto>}></Route>
-              <Route path="editarreserva/:id" element={<FormularioReserva titulo={'Modificar Reserva'}></FormularioReserva>}></Route>
-          </Route>
-          <Route path="/reserva/:id" element={<ReservaCancha  usuarioAdmin={usuarioAdmin}></ReservaCancha>}></Route>
-          <Route path="*" element={<Inicio></Inicio>}></Route>
-        </Routes>
-      </main>
-      <Footer></Footer>
-    </BrowserRouter>  
+              <Route path="crearusuario" element={<FormularioUsuario titulo={"Usuario Nuevo"}></FormularioUsuario>}></Route>
+              <Route path="editarusuario/:id" element={<FormularioUsuario titulo={"Modificar Usuario"}></FormularioUsuario>}></Route>
+              <Route path="crearcancha" element={<FormularioCancha titulo={"Cancha Nueva"}></FormularioCancha>}></Route>
+              <Route path="editarcancha/:id" element={<FormularioCancha titulo={"Modificar Cancha"}></FormularioCancha>}></Route>
+              <Route path="crearproducto" element={<FormularioProducto titulo={"Producto Nuevo"}></FormularioProducto>}></Route>
+              <Route path="editarproducto/:id" element={<FormularioProducto titulo={"Modificar Producto"}></FormularioProducto>}></Route>
+              <Route path="editarreserva/:id" element={<FormularioReserva titulo={"Modificar Reserva"}></FormularioReserva>}></Route>
+            </Route>
+            <Route path="/reserva/:id" element={<ReservaCancha usuarioAdmin={usuarioAdmin}></ReservaCancha>}></Route>
+            <Route path="*" element={<Inicio></Inicio>}></Route>
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,27 +1,34 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import {NavLink, Link, useNavigate } from 'react-router';
-import FullCanchaLogo from '../../assets/logo-canchasfull-nav.png'
-import { Button } from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink, Link, useNavigate } from "react-router";
+import FullCanchaLogo from "../../assets/logo-canchasfull-nav.png";
+import { Button } from "react-bootstrap";
 
-function Menu({usuarioAdmin, setUsuarioAdmin}) {
-  const navegacion = useNavigate()
-  const logout = () =>{
-    setUsuarioAdmin({})
-    navegacion('/')
-  }
+function Menu({ usuarioAdmin, setUsuarioAdmin }) {
+  const navegacion = useNavigate();
+  const logout = () => {
+    setUsuarioAdmin({});
+    navegacion("/");
+  };
   return (
     <Navbar expand="lg" className="navbar ps-4 pe-5">
       <Container fluid>
-        <Navbar.Brand as={Link} to={"/"} className='fw-bold text-white'><img src={FullCanchaLogo} alt="full cancha logo" className='logo-nav img-fluid'/></Navbar.Brand>
+        <Navbar.Brand as={Link} to={"/"} className="fw-bold text-white">
+          <img src={FullCanchaLogo} alt="full cancha logo" className="logo-nav img-fluid" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to={"/catalogo"} className='nav-link fw-bold text-white rounded px-2'><i 
-              className="bi bi-bag-fill fs-4 me-1"></i>Catalogo</Nav.Link>
-            <Nav.Link as={Link} to={"/carrito"} className='nav-link fw-bold text-white rounded px-2'><i 
-                className="bi bi-cart-plus-fill fs-4 me-1"></i>Carrito</Nav.Link>
+            <Nav.Link as={Link} to={"/"} className="nav-link fw-bold text-white rounded px-2">
+              <i class="bi bi-house-fill fs-4 me-1"></i>Inicio
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/productos"} className="nav-link fw-bold text-white rounded px-2">
+              <i className="bi bi-bag-fill fs-4 me-1"></i>Catalogo
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/carrito"} className="nav-link fw-bold text-white rounded px-2">
+              <i className="bi bi-cart-plus-fill fs-4 me-1"></i>Carrito
+            </Nav.Link>
             {usuarioAdmin.token && usuarioAdmin.rol === "staff" ? (
               <>
                 <NavLink className="nav-link fw-bold text-white rounded px-2" to={"/administrador"}>
@@ -37,7 +44,7 @@ function Menu({usuarioAdmin, setUsuarioAdmin}) {
                 Cerrar Sesión
               </Button>
             ) : (
-              <Nav.Link as={Link} to={"/login"} className='nav-link fw-bold text-white rounded px-2'>
+              <Nav.Link as={Link} to={"/login"} className="nav-link fw-bold text-white rounded px-2">
                 <i className="bi bi-person-circle fs-4 me-2"></i>Iniciar Sesión
               </Nav.Link>
             )}
